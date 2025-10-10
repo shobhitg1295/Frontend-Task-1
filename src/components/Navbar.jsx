259748359de7dc8c1495f5efb43d100ecbb401a5
shobhitg1295 <shobhitg1295@gmail.com>
2025-10-11 03:19:45 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-indigo-900 text-white py-2'>
+    <nav className='flex justify-between bg-gray-700 text-white py-2'>
         <div className="logo">
             <span className='font-bold text-xl mx-8'>My Check List</span>
         </div>
@@ -15,4 +15,5 @@ const Navbar = () => {
 }
 
 export default Navbar
+
 
