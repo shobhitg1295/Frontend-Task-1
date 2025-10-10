2ee562f6628d6a6515bf30c9376041a175795372
shobhitg1295 <shobhitg1295@gmail.com>
2025-10-11 03:13:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -76,17 +76,17 @@ function App() {
        <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-gray-700 text-white min-h-[80vh] md:w-1/2">
         <h1 className='font-bold text-center text-xl'>My Check List - Manage your tasks at one place</h1>
          <div className="addTodo my-5 flex flex-col gap-4">
-          <h2 className='text-lg font-bold text-center'>Add a Todo Task:</h2>
+          <h2 className='text-lg font-bold text-center'>Add a Todo Task :</h2>
           <input  onChange={handleChange} value={todo} type="text" className='w-full rounded-full text-black px-5 py-1' />
-          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-blue-500 hover:bg-blue-900 disabled:bg-blue-300 p-2 py-1 text-sm font-bold text-black rounded-md'>ADD</button>
+          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-blue-500 hover:bg-blue-900 disabled:bg-blue-300 p-2 py-1 text-sm font-bold rounded-md'>ADD</button>
          </div>
-         <input className='my-4' onChange={toggleFinished} type="checkbox" checked={showFinished} /> Show Finished
-         <h2 className='text-lg font-bold'>Your Todo Tasks:</h2>
+         <input className='my-4' onChange={toggleFinished} type="checkbox" checked={showFinished}  /> Show Finished
+         <h2 className='text-lg font-bold'>Your Todo Tasks :</h2>
          <div className="todos">
           {todos.length ===0 && <div className='m-5'>No Todos to display !</div> }
           {todos.map(item=>{
  
-          return (showFinished || !item.isCompleted) && <div key={item.id} className={"todo flex md:w-1/2 my-3 justify-between"}>
+          return (showFinished || !item.isCompleted) && <div key={item.id} className={"todo flex md:w-4/5 my-3 justify-between"}>
             <div className='flex gap-5'> 
             <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id="" />
             <div className={item.isCompleted?"line-through":""}>{item.todo}</div>
