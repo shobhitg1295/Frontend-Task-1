0cff27c6a6e1f9ba6dffed0de36d5ebe9e0b0fe6
shobhitg1295 <shobhitg1295@gmail.com>
2025-10-11 02:03:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -76,12 +76,12 @@ function App() {
        <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-1/2">
         <h1 className='font-bold text-center text-xl'>My Check List - Manage your tasks at one place</h1>
          <div className="addTodo my-5 flex flex-col gap-4">
-          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <h2 className='text-lg font-bold'>Add a Todo task</h2>
           <input  onChange={handleChange} value={todo} type="text" className='w-full rounded-full px-5 py-1' />
           <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-800 hover:bg-violet-950 disabled:bg-violet-500 p-2 py-1 text-sm font-bold text-white rounded-md'>Save</button>
          </div>
          <input className='my-4' onChange={toggleFinished} type="checkbox" checked={showFinished} /> Show Finished
-         <h2 className='text-lg font-bold'>Your Todos</h2>
+         <h2 className='text-lg font-bold'>Your Todo tasks</h2>
          <div className="todos">
           {todos.length ===0 && <div className='m-5'>No Todos to display</div> }
           {todos.map(item=>{
