e7496c46dede9acb030c52aa8e4edcb3c5c27a82
shobhitg1295 <shobhitg1295@gmail.com>
2025-10-11 01:50:17 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className='flex justify-between bg-indigo-900 text-white py-2'>
         <div className="logo">
-            <span className='font-bold text-xl mx-8'>iTask</span>
+            <span className='font-bold text-xl mx-8'>My Check List</span>
         </div>
       <ul className="flex gap-8 mx-9">
         <li className='cursor-pointer hover:font-bold transition-all'>Home</li>
@@ -15,3 +15,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
